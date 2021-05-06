dedb9245c4a1f5912c0a653163e9f743c1e9bc24
mm0102-dev <superdev678@gmail.com>
2021-05-06 19:44:09 +1000

    1

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -1,19 +1,28 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class AnalyticsController extends Controller
 {
     public function __construct()
     {
         
     }
-
-    
+    //all analytics
     public function index()
     {
         return view('users.analytics.index');
     }
+    
+    // long analytics
+    public function long_index()
+    {
+        return view('users.analytics.long');
+    }
+    
+    //short analytics
+    public function short_index()
+    {
+        return view('users.analytics.short');
+    }
+    
 }
