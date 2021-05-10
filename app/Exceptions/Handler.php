0763a2a0c5ed118718b3956e0a072156d163b8b1
prodevdmytro <75682729+prodevdmytro@users.noreply.github.com>
2021-05-10 17:27:15 +1000

    Update handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -40,14 +40,4 @@ class Handler extends ExceptionHandler
 
     }
 
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-        if ($request->is('admin') || $request->is('admin/*')) {
-            return redirect()->guest('admin/login');
-        }
-        return redirect()->guest(route('login'));
-    }
 }
