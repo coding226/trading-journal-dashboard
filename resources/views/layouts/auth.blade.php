d05bceaaec827875f0d3141a4ca2188d12067d18
prodevdmytro <75682729+prodevdmytro@users.noreply.github.com>
2021-05-25 20:56:40 +1000

    register bug fix

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,10 +5,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="pixelstrap">
-
+    <meta name="description" content="The only trading journal that helps you find your buddy.">
+    <meta name="keywords" content="trading buddy, trading, buy, sell, long, short, web app">
+    <meta name="author" content="Milos">
+    
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,11 +5,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="The only trading journal that helps you find your buddy.">
-    <meta name="keywords"
-        content="trading buddy, trading, buy, sell, long, short, web app">
-    <meta name="author" content="pixelstrap">
+    <meta name="description" content="The only trading journal that helps you find your buddy.">
+    <meta name="keywords" content="trading buddy, trading, buy, sell, long, short, web app">
+    <meta name="author" content="Milos">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ url('assets/images/favicon.png') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ url('assets/images/favicon.png') }}" type="image/x-icon">
