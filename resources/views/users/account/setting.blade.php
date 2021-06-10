c65ec5ec5e91321f210f8117389bd1b6a911206e
prodevdmytro <75682729+prodevdmytro@users.noreply.github.com>
2021-06-10 21:18:40 +1000

    Update setting.blade.php

--- resources/views/users/account/setting.blade.php
+++ resources/views/users/account/setting.blade.php
@@ -238,6 +238,43 @@
                         </form>
                     </div>
                     <div class="col-xl-4">
+                        <form method="POST" onsubmit="return false;" class="card" id="startingbalform">
+                            @csrf
+                            <div class="card-header">
+                                <h4 class="card-title mb-0">Starting Balance</h4>
+                                <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
+                            </div>
+                            <div class="card-body">
+                                @php
+                                    $subusers = DB::table('subusers')->where('user_id', Auth::user()->id)->get();
+                                @endphp
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Account</label>
+                                            <select class="form-select" id="stsubuser" name="stsubuser">
+                                                @foreach($subusers as $subuser)
+                                                @if($subuser-> id == Auth::user()->current_subuser)
+                                                <option value="{{ $subuser-> id }}" selected>{{ $subuser-> username }}</option>
+                                                @else
+                                                <option value="{{ $subuser-> id }}">{{ $subuser-> username }}</option>
+                                                @endif
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Amount</label>
+                                            <input class="form-control" type="number" id="stamount" name="stamount" placeholder="Enter in a starting balance">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer text-end">
+                                <button class="btn btn-primary" id="startingbal">Update</button>
+                            </div>
+                        </form>
                         <form method="POST" onsubmit="return false;" class="card" id="depwithform">
                         {{-- <form method="POST" action="{{ Route('user.depositwithdrawal') }}" class="card"> --}}
                             @csrf
@@ -283,43 +320,6 @@
                             </div>
                             <div class="card-footer text-end">
                                 <button class="btn btn-primary" id="depwith">Update</button>
-                            </div>
-                        </form>
-                        <form method="POST" onsubmit="return false;" class="card" id="startingbalform">
-                            @csrf
-                            <div class="card-header">
-                                <h4 class="card-title mb-0">Starting Balance</h4>
-                                <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
-                            </div>
-                            <div class="card-body">
-                                @php
-                                    $subusers = DB::table('subusers')->where('user_id', Auth::user()->id)->get();
-                                @endphp
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="mb-3">
-                                            <label class="form-label">Account</label>
-                                            <select class="form-select" id="stsubuser" name="stsubuser">
-                                                @foreach($subusers as $subuser)
-                                                @if($subuser-> id == Auth::user()->current_subuser)
-                                                <option value="{{ $subuser-> id }}" selected>{{ $subuser-> username }}</option>
-                                                @else
-                                                <option value="{{ $subuser-> id }}">{{ $subuser-> username }}</option>
-                                                @endif
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="mb-3">
-                                            <label class="form-label">Amount</label>
-                                            <input class="form-control" type="number" id="stamount" name="stamount" placeholder="Enter in a starting balance">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-footer text-end">
-                                <button class="btn btn-primary" id="startingbal">Update</button>
                             </div>
                         </form>
                     </div>
