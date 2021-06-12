829b01ecfa8ff86027a795812a70de0813c236e9
prodevdmytro <75682729+prodevdmytro@users.noreply.github.com>
2021-06-13 00:06:36 +1000

    Update pdfview.blade.php

--- resources/views/users/trade/pdfview.blade.php
+++ resources/views/users/trade/pdfview.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="{{ url('assets/css/vendors/bootstrap.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="{{ url('assets/css/vendors/bootstrap.css') }}"> -->
     <style>
         .preview img{
             max-width: 100%;
